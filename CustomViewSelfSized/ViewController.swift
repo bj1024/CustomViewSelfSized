87fefb6c2f6cdf7a28e680dcd054d58e982b2604
H.K <16552133+bj1024@users.noreply.github.com>
2019-09-09 16:50:19 +0900

    sub view intrinsicContentSize test.

--- CustomViewSelfSized/ViewController.swift
+++ CustomViewSelfSized/ViewController.swift
@@ -6,9 +6,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var myView: MyView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
+    myView.startSelfSizeChange()
   }
 
 
